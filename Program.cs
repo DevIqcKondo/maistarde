be479f6db0d16b5ed0ced4a66af75e01cb9b30f4
Isadora Kondo <kondoiqcdev@gmail.com>
2021-05-21 19:00:26 -0300

    Execução do exercício

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace maistarde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime horaatual = DateTime.Now;
+            TimeSpan acreshora = new TimeSpan(6, 30, 0);
+            Console.Write("Agora são ");
+            Console.Write(horaatual);
+            Console.WriteLine();
+            Console.Write("Mais tarde será ");
+            Console.Write(horaatual+acreshora);
+            Console.WriteLine();
+        }
+    }
+}
